fd3975e06dc14318dbca685de06c81262ce1bcc9
Om Kadu <kaduom444@gmail.com>
2023-08-23 22:12:33 +0530

    fool

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import './index.css';
 
 
-let curDate=new Date(2023,5,5,19);
+let curDate=new Date();
 curDate=curDate.getHours();
 let greeting='';
  const cssStyle={
