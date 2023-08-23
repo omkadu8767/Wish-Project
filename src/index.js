c6d7b486ef197797733c7d5d0510f1bbb07b7ea0
Om Kadu <kaduom444@gmail.com>
2023-08-24 00:41:47 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './App';
 import './index.css';
 
 
-let curDate=new Date();
-curDate=curDate.getHours();
-let greeting='';
- const cssStyle={
- color: 'green'
- };
 
-
-
-if(curDate>=1 && curDate<12){
-  greeting='Good Morning';
-  cssStyle.color='green';
-} else if(curDate>=12 && curDate<19){
-  greeting='Good Afternoon';
-  cssStyle.color='Orange';
-}
-else{
-  greeting='Good Night';
-  cssStyle.color='Black';
-
-}
 ReactDOM.render(
-  <>
-  <div>
-  <h1>Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
-  </div>
-  </>,
+  <App/>,
   document.getElementById('root')
 );
